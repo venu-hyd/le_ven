7fef3b593f86dc4b4edeb9298aee43c5105403ce
Venugopal <venugopal_vadapalli@hotmail.com>
2017-12-19 17:39:30 +0530

    new changes

--- www/vc/js/url.js
+++
@@ -1,3 +0,0 @@
-var url = 'http://loteasy.com:8080/OMS'; 
-var item_image_url='http://loteasy.com:8080/TEST_IMAGES/'; // Predefined url for item images.
-var image_jsp_path='http://loteasy.com:8080/OMS/ajax_file.jsp';
